9aa943b7284d8a05b29fbd7b039a218586f8b0cf
davidpinkevich <davidpinkevich95@gmail.com>
2024-01-24 22:26:16 +0300

    feat: create slice

---
+++ src/redux/slices/index.ts
@@ -0,0 +1,37 @@
+import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
+import { TypePhoto } from "types";
+
+interface TypesInitialState {
+  search: string;
+  typeSearch: string;
+  tooltip: boolean;
+  photos: Array<TypePhoto>;
+}
+
+const initialState: TypesInitialState = {
+  search: "",
+  typeSearch: "photo",
+  tooltip: false,
+  photos: [],
+};
+
+const searchSlice = createSlice({
+  name: "pexels",
+  initialState,
+  reducers: {
+    hiddenTooltip: (state, actions: PayloadAction<boolean>) => {
+      state.tooltip = actions.payload;
+    },
+    changeType: (state, actions: PayloadAction<string>) => {
+      state.typeSearch = actions.payload;
+    },
+  },
+  selectors: {
+    getState: (state) => state,
+  },
+});
+
+export const { hiddenTooltip, changeType } = searchSlice.actions;
+export const { getState } = searchSlice.selectors;
+export default searchSlice.reducer;
