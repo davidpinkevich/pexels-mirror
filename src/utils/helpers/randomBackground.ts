7e82cbd8c1f085b5d2db1273b148ed75cb7de5cc
davidpinkevich <davidpinkevich95@gmail.com>
2024-02-13 22:56:11 +0300

    fix: change logic random

--- src/utils/helpers/randomBackground.ts
+++ src/utils/helpers/randomBackground.ts
@@ -1,9 +1,7 @@
 import { PHOTOS_FOR_PAGE } from "utils/constants";
-import { TypePhotos } from "components/Photos/Items/Items.types";
 
-const randomBackground = (data: TypePhotos | null) => {
-  const number = Math.floor(Math.random() * PHOTOS_FOR_PAGE);
-  if (data) return data.photos[number];
+const randomNumber = (): number => {
+  return Math.floor(Math.random() * PHOTOS_FOR_PAGE);
 };
 
-export { randomBackground };
+export { randomNumber };
