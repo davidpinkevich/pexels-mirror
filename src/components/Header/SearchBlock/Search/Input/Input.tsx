6665e2cd373c516c5884050e7956ac4dc59e3961
davidpinkevich <davidpinkevich95@gmail.com>
2024-02-13 23:05:54 +0300

    feat: add input search

---
+++ src/components/Header/SearchBlock/Search/Input/Input.tsx
@@ -0,0 +1,24 @@
+import { forwardRef, ForwardedRef, KeyboardEvent } from "react";
+import { useNavigate } from "react-router-dom";
+import "./Input.scss";
+
+const Input = forwardRef((_, ref: ForwardedRef<HTMLInputElement>) => {
+  const navigate = useNavigate();
+  const handleClick = (event: KeyboardEvent<HTMLInputElement>) => {
+    if (ref && "current" in ref && ref.current?.value) {
+      if (event.code === "Enter") navigate(`search/${ref.current?.value}/`);
+    }
+  };
+  return (
+    <div className="search__block-input">
+      <input
+        ref={ref}
+        onKeyDown={handleClick}
+        type="text"
+        placeholder="Поиск бесплатных изображений"
+      />
+    </div>
+  );
+});
+
+export default Input;
