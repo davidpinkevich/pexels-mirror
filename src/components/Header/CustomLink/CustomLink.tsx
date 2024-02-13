e5d91dfdfa1861f9b7dc517a1e6e581c12be7191
davidpinkevich <davidpinkevich95@gmail.com>
2024-02-13 22:55:07 +0300

    feat: add open new page

--- src/components/Header/CustomLink/CustomLink.tsx
+++ src/components/Header/CustomLink/CustomLink.tsx
@@ -4,7 +4,7 @@ import { TypeCustomLink } from "./CustomLink.types";
 const CustomLink = ({ url, name }: TypeCustomLink) => {
   return (
     <div className="header__link-author">
-      <a href={url}>
+      <a href={url} target="blank">
         <p>
           Автор фото - <span>{name}</span>
         </p>
