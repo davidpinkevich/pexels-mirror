8630a8c69b5bbcc71ba3f77d144b4a006b1db494
davidpinkevich <davidpinkevich95@gmail.com>
2024-02-13 23:02:22 +0300

    feat: add logic sticky for logo

--- src/components/Header/Logo/Logo.tsx
+++ src/components/Header/Logo/Logo.tsx
@@ -2,13 +2,13 @@ import { LINKS } from "utils/constants";
 import img from "../../../assets/icons/logo.svg";
 import "./Logo.scss";
 
-const Logo = () => {
+const Logo = ({ sticky }: { sticky: boolean }) => {
   return (
     <a href={LINKS.MAIN} className="header__logo logo">
       <div className="logo__img">
         <img src={img} alt="logo" />
       </div>
-      <p className="logo__name">Pexels</p>
+      <p className={sticky ? "logo__name-black" : "logo__name-white"}>Pexels</p>
     </a>
   );
 };
