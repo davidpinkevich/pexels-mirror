64ca566b69ae18494df2d7061fb50216fa26e4de
davidpinkevich <davidpinkevich95@gmail.com>
2024-02-13 23:00:15 +0300

    feat: add types for main page

---
+++ src/pages/MainPage/MainPage.types.ts
@@ -0,0 +1,10 @@
+import { TypePhotos } from "components/Photos/Items/Items.types";
+
+export type TypeData = {
+  photos: TypePhotos;
+};
+
+export type LogicTypesMain = {
+  photos: TypePhotos;
+  sticky: boolean;
+};
