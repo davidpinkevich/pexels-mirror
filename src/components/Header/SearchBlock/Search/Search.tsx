b5310e5bcb702edb061b7033953bae79d468355c
davidpinkevich <davidpinkevich95@gmail.com>
2024-02-13 23:41:40 +0300

    feat: add search

---
+++ src/components/Header/SearchBlock/Search/Search.tsx
@@ -0,0 +1,18 @@
+import { useRef } from "react";
+import ButtonInfo from "./ButtonInfo/ButtonInfo";
+import Input from "./Input/Input";
+import ButtonFind from "./ButtonFind/ButtonFind";
+import "./Search.scss";
+
+const Search = ({ sticky }: { sticky: boolean }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  return (
+    <div className="search__block">
+      <ButtonInfo />
+      <Input ref={inputRef} />
+      <ButtonFind ref={inputRef} />
+    </div>
+  );
+};
+
+export default Search;
