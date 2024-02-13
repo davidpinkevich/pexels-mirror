6e62532cef844738d7e25ff9d443cf42c2979946
davidpinkevich <davidpinkevich95@gmail.com>
2024-02-13 23:03:17 +0300

    fix: change random bg

--- src/components/Header/ImageBg/ImageBg.tsx
+++ src/components/Header/ImageBg/ImageBg.tsx
@@ -1,17 +1,21 @@
+import { memo } from "react";
 import CustomLink from "../CustomLink/CustomLink";
 import { TypeHeader } from "../Header.types";
 
-const ImageBg = ({ photo }: TypeHeader) => {
+const ImageBg = memo(({ random }: TypeHeader) => {
   return (
     <>
-      {photo && (
+      {random && (
         <>
-          <img src={photo?.src.landscape} alt={photo?.alt ?? "background"} />
-          <CustomLink url={photo.photographer_url} name={photo.photographer} />
+          <img src={random?.src.landscape} alt={random?.alt ?? "background"} />
+          <CustomLink
+            url={random.photographer_url}
+            name={random.photographer}
+          />
         </>
       )}
     </>
   );
-};
+});
 
 export default ImageBg;
