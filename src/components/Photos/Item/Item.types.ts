1dc17a8d9016d08222315df8461d0ba4fe0c28fa
davidpinkevich <davidpinkevich95@gmail.com>
2024-02-13 23:08:17 +0300

    feat: add types item photo

---
+++ src/components/Photos/Item/Item.types.ts
@@ -0,0 +1,14 @@
+export type TypePhoto = {
+  id: number;
+  avg_color: string;
+  photographer: string;
+  photographer_url: string;
+  url: string;
+  alt: string;
+  width: number;
+  height: number;
+  src: {
+    landscape: string;
+    large: string;
+  };
+};
